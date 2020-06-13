1e7f226497f49752d232f614f8dadb54cb979e29
coral <coralscharf@gmail.com>
2020-06-13 20:01:35 +0300

    update find pairs

--- php/find_pairs.php
+++ php/find_pairs.php
@@ -10,68 +10,69 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 $final_pairs_order = array();
 
-$sql="select [order] as rel_order
+$sql1="select [order] as rel_order
 from exp_pairs                       
 where exp_id = ".$exp_id." and realConf = 1";
 
-$getResults= sqlsrv_query($conn, $sql);
-if ($getResults == FALSE)
+$getResults1= sqlsrv_query($conn, $sql1);
+if ($getResults1 == FALSE)
 {
     echo 'err';
     die();
 }
 
 $true_pair_arr=array();
-while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+while ($row = sqlsrv_fetch_array($getResults1, SQLSRV_FETCH_ASSOC)) {
     if($row['rel_order'] != 1 and
         (($row['rel_order'] != 5 and $total_pairs<=10) or ($row['rel_order'] != 15 and $total_pairs>=10))){
         array_push($true_pair_arr, $row['rel_order']);
     }
 }
 
-$random_true_keys=array_rand($true_pair_arr, $num_of_true_pairs);
+$random_true_keys1=array_rand($true_pair_arr, $num_of_true_pairs);
 
-foreach ($random_true_keys as $key) {
+foreach ($random_true_keys1 as $key) {
     echo $true_pair_arr[$key];
     echo ' ';
     array_push($final_pairs_order, $true_pair_arr[$key]);
 }
 
 
-$sql="select [order] as rel_order                      
+$sql2="select [order] as rel_order                      
 from exp_pairs                       
 where exp_id = ".$exp_id." and realConf = 0";
 
-$getResults= sqlsrv_query($conn, $sql);
-if ($getResults == FALSE)
+$getResults2= sqlsrv_query($conn, $sql2);
+if ($getResults2 == FALSE)
 {
     echo 'err';
     die();
 }
 
 $false_pair_arr=array();
-while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+while ($row = sqlsrv_fetch_array($getResults2, SQLSRV_FETCH_ASSOC)) {
     if($row['rel_order'] != 1 and
         (($row['rel_order'] != 5 and $total_pairs<=10) or ($row['rel_order'] != 15 and $total_pairs>=10))){
         array_push($false_pair_arr, $row['rel_order']);
     }
 }
-sqlsrv_free_stmt($getResults);
+sqlsrv_free_stmt($getResults2);
 
-$random_false_keys=array_rand($false_pair_arr, $num_of_false_pairs);
-foreach ($random_false_keys as $key) {
+$random_false_keys2=array_rand($false_pair_arr, $num_of_false_pairs);
+foreach ($random_false_keys2 as $key) {
     echo $false_pair_arr[$key];
     echo ' ';
     array_push($final_pairs_order, $false_pair_arr[$key]);
 }
 
 shuffle($final_pairs_order);
-echo $final_pairs_order;
-echo '<br>';
+
 $final_res = array();
 array_push($false_pair_arr, 1);
 $i = 2;
 foreach ($final_pairs_order as $order) {
+    echo " order ";
+    echo $order;
     if(($i = 15 and $total_pairs>=10) or ($i = 5 and $total_pairs>=10)){
         array_push($false_pair_arr, $i);
     }
